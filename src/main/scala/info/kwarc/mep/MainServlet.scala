4a6633b2f5f270de576d4cc9ad62d004cafa969a
aoripov <akbarzoripov@gmail.com>
2015-08-07 20:38:25 +0200

    Add route to deal with queries

--- src/main/scala/info/kwarc/mep/MainServlet.scala
+++ src/main/scala/info/kwarc/mep/MainServlet.scala
@@ -5,9 +5,24 @@ import scalate.ScalateSupport
 
 class MainServlet extends MepStack {
 
+  val backend = new MMTBackend()
+  
   get("/") {
   	contentType="text/html"
     jade("/index", "layout" -> "WEB-INF/templates/layouts/default.jade", "title" -> "Main | MEP")
   }
+
+  post("/query") {
+  	val response = scala.xml.XML.loadString(params("data"));
+  	contentType="html"
+  	response match {
+  		case <oeis>{content}</oeis> =>
+        
+        val pres = backend.getPresentation(content.toString, "oeis-omdoc")   
+        pres
+  		case r => <b>No presentation {r}</b>
+  	}
+  }
+
 
 }
